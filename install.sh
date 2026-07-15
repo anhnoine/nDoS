f956f9dd59e9b4a65aea505dd2a0e9504fb9dbc3
anhnoine <anhnoine@outlook.com>
2026-07-15 12:27:17 +0700

    Add installation script for N-DoS tool
    This script installs the N-DoS tool, including necessary plugins and dependencies.

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+clear
+apt install git -y
+clear
+echo ""
+echo "  ======================================="
+echo "    N-DoS installer"
+echo "    repo: anhnoine/nDoS"
+echo "  ======================================="
+echo ""
+sleep 5
+bash <(curl -s https://raw.githubusercontent.com/anhnoine/n-manios/refs/heads/main/nPlugins/nplugin_install.sh)
+rm nSocks.c
+clear
+wget https://raw.githubusercontent.com/anhnoine/n-manios/refs/heads/main/nPlugins/plugins/nSocks.c
+wget https://raw.githubusercontent.com/anhnoine/nDoS/refs/heads/main/tools/nDoS.mno
+nplugin install -f nSocks.c
+rm nSocks.c
+clear
+echo ""
+echo "  ======================================="
+echo "    N-DoS: SAN SANG!"
+echo "  ======================================="
+echo ""
+echo "  mno nDoS.mno (ip:port) [thread]"
+echo ""
