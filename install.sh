13d855353846f3f154730fa54de1d5c485757d96
anhnoine <anhnoine@outlook.com>
2026-07-16 14:13:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ wget https://raw.githubusercontent.com/anhnoine/n-manios/refs/heads/main/nPlugin
 wget https://raw.githubusercontent.com/anhnoine/n-manios/refs/heads/main/nPlugins/plugins/n-args.c
 wget https://raw.githubusercontent.com/anhnoine/nDoS/refs/heads/main/tools/nDoS.mno
 nplugin install -f nSocks.c
+nplugin install -f n-args.c
 rm nSocks.c
 rm n-args.c
 clear
